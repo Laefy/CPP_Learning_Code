13f3521fa598616353ff01d555f1b444eafbf371
Laefy <laefy22@gmail.com>
2021-01-02 06:28:31 +0100

    Add solutions for Chapter 1.

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,98 @@
-// Implement the program here.
+#include <iostream>
+#include <string>
+
+// #define READ
+// #define WHILE
+// #define DO_WHILE
+// #define FOR
+// #define STRING
+#define USING_NS
+
+#ifdef READ
+int main()
+{
+    int number = 0;
+    std::cin >> number;
+    std::cout << "Craow " << number << "!" << std::endl;
+    return 0;
+}
+#endif
+
+#ifdef WHILE
+int main()
+{
+    while (true)
+    {
+        int number = 0;
+        std::cin >> number;
+        std::cout << "Craow " << number << "!" << std::endl;
+
+        if (number == -1)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
+#endif
+
+#ifdef DO_WHILE
+int main()
+{
+    int number = 0;
+
+    do
+    {
+        std::cin >> number;
+        std::cout << "Craow " << number << "!" << std::endl;
+    }
+    while (number != -1);
+
+    return 0;
+}
+#endif
+
+#ifdef FOR
+int main()
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        int number = 0;
+        std::cin >> number;
+        std::cout << "Craow " << number << "!" << std::endl;
+    }
+
+    return 0;
+}
+#endif
+
+#ifdef STRING
+int main()
+{
+    for (int i = 0; i < 10; ++i)
+    {
+        std::string word;
+        std::cin >> word;
+        std::cout << "Craow " << word << "!" << std::endl;
+    }
+
+    return 0;
+}
+#endif
+
+#ifdef USING_NS
+int main()
+{
+    using namespace std;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        string word;
+        cin >> word;
+        cout << "Craow " << word << "!" << endl;
+    }
+
+    return 0;
+}
+#endif
--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
+// #define STATIC_VALUES
+// #define FOREACH
+// #define STATIC_SIZE
+// #define DYNAMIC
+#define VECTOR
+
+#ifdef STATIC_VALUES
 int main()
 {
     int array[] = { 0, 1, 2, 3 };
@@ -11,3 +20,106 @@ int main()
 
     return 0;
 }
+#endif
+
+#ifdef FOREACH
+int main()
+{
+    int array[] = { 0, 1, 2, 3 };
+
+    for (int value : array)
+    {
+        std::cout << value << std::endl;
+    }
+
+    return 0;
+}
+#endif
+
+#ifdef STATIC_SIZE
+int main()
+{
+    int array[50] = {};
+
+    for (int i = 0; i < 50; ++i)
+    {
+        // Array indices start at 0, so we need to use (i+1) if we want the series to start at 1.
+        array[i] = i+1;
+    }
+
+    for (int value : array)
+    {
+        std::cout << value << std::endl;
+    }
+
+    return 0;
+}
+#endif
+
+#ifdef DYNAMIC
+int main(int argc, char** argv)
+{
+    // First argument is always the command name used to launch the program.
+    if (argc < 2)
+    {
+        std::cerr << "Expected argument for array size." << std::endl;
+        return -1;
+    }
+
+    int length = std::stoi(argv[1]);
+    if (length <= 0)
+    {
+        std::cerr << "Expected strictly positive value for array size." << std::endl;
+        return -1;
+    }
+
+    int* array = new int[length] {};
+
+    for (int i = 0; i < length; ++i)
+    {
+        array[i] = i+1;
+    }
+
+    for (int i = 0; i < length; ++i)
+    {
+        std::cout << array[i] << std::endl;
+    }
+
+    delete[] array;
+
+    return 0;
+}
+#endif
+
+#ifdef VECTOR
+int main(int argc, char** argv)
+{
+    // First argument is always the command name used to launch the program.
+    if (argc < 2)
+    {
+        std::cerr << "Expected argument for array size." << std::endl;
+        return -1;
+    }
+
+    int length = std::stoi(argv[1]);
+    if (length <= 0)
+    {
+        std::cerr << "Expected strictly positive value for array size." << std::endl;
+        return -1;
+    }
+
+    std::vector<int> array;
+
+    for (int i = 0; i < length; ++i)
+    {
+        array.emplace_back(i+1);
+    }
+
+    for (int value : array)
+    {
+        std::cout << value << std::endl;
+    }
+
+    return 0;
+}
+#endif
