1780b82fb32aa2003f35f73c6341bbde6515e1fe
Celine NOEL <laefy22@gmail.com>
2022-01-24 22:44:58 +0100

    Update Tests.cpp

--- tp-03/Pokemons/Tests.cpp
+++ tp-03/Pokemons/Tests.cpp
@@ -37,7 +37,7 @@
 //     REQUIRE(pokemon.name() == "Pikachu");
 // }
 
-// TEST_CASE("Poke centers are constructed with 0 pokemon inside")
+// TEST_CASE("PCs are constructed with 0 pokemon inside")
 // {
 //     PC pc;
 
@@ -45,7 +45,7 @@
 //     REQUIRE(pokemons.empty() == true);
 // }
 
-// TEST_CASE("A Pokemon can be put inside the poke center")
+// TEST_CASE("A Pokemon can be put inside the PC")
 // {
 //     PC pc;
 
@@ -57,7 +57,7 @@
 //     REQUIRE(pokemons[0]->name() == "Pikachu");
 // }
 
-// TEST_CASE("Trainers are constructed with their name and they know the poke center that they are associated with")
+// TEST_CASE("Trainers are constructed with their name and they know the PC where their Pokemons get sent")
 // {
 //     PC      pc;
 //     Trainer sacha { "Sacha", pc };
