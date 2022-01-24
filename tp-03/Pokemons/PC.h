87f688aca7a011b3ff5948f4066c46baf7e03315
igel-kun <mathiaswe@gmx.de>
2022-01-24 16:25:48 +0100

    Update PC.h

--- tp-03/Pokemons/PC.h
+++ tp-03/Pokemons/PC.h
@@ -1,5 +1,6 @@
 #pragma once
 
-// Pokemon are automatically stored there if the trainer doesn't have enough space on them.
+// A poke center is associated with trainers, whose pokemon can be stored in the center if needed (for example when they have more pokemon than pokeballs).
+// When a pokemon is transferred to the center, the center takes ownership.
 class PC
 {};
