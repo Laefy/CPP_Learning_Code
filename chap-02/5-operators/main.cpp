5661d330ffda24686cdf1fa3a6c2ee6ef5c6c2f2
Laefy <laefy22@gmail.com>
2024-01-14 22:56:06 +0100

    Wip chap-02

---
+++ chap-02/5-operators/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // Polygon polygon;
+
+    // polygon.add_vertex(2, 3);
+    // polygon.add_vertex(4, 5);
+    // polygon.add_vertex(6, 7);
+
+    // auto vertex = polygon[1];
+    // std::cout << "(" << vertex.first << "," << vertex.second << ")" << std::endl; // -> (4,5)
+
+    // std::cout << polygon << std::endl;
+
+    return 0;
+}
